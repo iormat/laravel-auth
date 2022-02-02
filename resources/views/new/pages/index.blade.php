8df682aeef6d29ba44bb256847747dc5b4bb3dc3
iormat <iorio436@outlook.it>
2022-02-02 17:58:13 +0100

    registration form

--- resources/views/new/pages/index.blade.php
+++ resources/views/new/pages/index.blade.php
@@ -1,4 +1,30 @@
 @extends('new.layouts.main-layout')
 @section('content')
-    prova
+    {{-- login --}}
+    <div class="my_container">
+        <h2>Registration</h2>
+        <form action="">
+            @csrf
+            @method('POST')
+
+            <div class="form-floating mb-3">
+                <label for="floatingInput">Username</label>
+                <input type="text" class="form-control" id="floatingInput" name="name" placeholder="Username">
+            </div>
+            <div class="form-floating mb-3">
+                <label for="floatingInput">Email address</label>
+                <input type="email" class="form-control" id="floatingInput" name="email" placeholder="name@example.com">
+            </div>
+            <div class="form-floating mb-3">
+                <label for="floatingPassword">Password</label>
+                <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password">
+            </div>
+            <div class="form-floating mb-3">
+                <label for="floatingPasswordConfirmation">Password Confirmation</label>
+                <input type="password" class="form-control" id="floatingPasswordConfirmation" name="password_confirmation" placeholder="Repeat Password">
+            </div>
+
+            <button class="btn btn-primary" name="submit">submit</button>
+        </form>
+    </div>
 @endsection
